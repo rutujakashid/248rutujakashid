4cdfc7a08de8c54c469e30856be9d20bfe546a83
rutujakashid <68296703+rutujakashid@users.noreply.github.com>
2020-07-18 17:18:52 +0530

    Add files via upload

---
+++ assignment 1c.py
@@ -0,0 +1,11 @@
+a=int(input("enter value for a="))
+b=int(input("enter value for b="))
+
+addition=a+b
+print(a,"+",b,"=",addition)
+
+subtraction=a-b
+print(a,"-",b,"=",subtraction)
+
+multiplication=a*b
+print(a,"*",b,"=",multiplication)
