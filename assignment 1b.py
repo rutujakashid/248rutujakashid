fcad8c3a022d0734194e09fc73da59655801ec9d
rutujakashid <68296703+rutujakashid@users.noreply.github.com>
2020-07-18 16:49:44 +0530

    Add files via upload

---
+++ assignment 1b.py
@@ -0,0 +1,34 @@
+
+list=[1,2,3,4]
+print(list)
+
+
+print("insert element")
+list.insert(6,7)
+print(list)
+
+print("remove element")
+list.remove(3)
+print(list)
+
+print("reverse the list")
+list.reverse()
+print(list)
+
+print("sort the list")
+list.sort()
+print(list)
+
+print("pop function on list")
+list.pop()
+print(list)
+
+
+print("append function on list")
+list.append(9) 
+print(list)
+
+
+
+
+
