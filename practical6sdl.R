01a3a19382242e18c5d1fa3d7dc711c32b466d2c
rutujakashid <68296703+rutujakashid@users.noreply.github.com>
2020-12-24 21:26:42 +0530

    Add files via upload

---
+++ practical6sdl.R
@@ -0,0 +1,28 @@
+#upload dataset
+mydata<-read.csv('C:\\Users\\DELL\\Documents\\Maharashtra-Table2007.csv')
+
+#show few rows of dataset
+mydata[1:10, ]
+
+#dimensions of dataset
+dim(mydata)
+
+#str shows variable types
+str(mydata)
+
+
+#summary of all columns of dataset
+summary(mydata)
+
+
+#checks the entire data set for NAs and return logical output
+is.na(mydata)
+colSums(is.na(mydata))
+#replace na to 0 in column breed.of.horse.malani
+mydata[is.na(mydata)] = 0
+
+#data visualization
+col_total<-mydata[,11]
+View(col_total)
+boxplot(col_total)
+
